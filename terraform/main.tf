6d081a370370a895a7f51b487ace6771afb996d2
Aleksandr Rogembaum <aleksandr.rogembaum@fin-frame.ru>
2026-09-15 13:46:32 +0300

    ISSUE-1: add infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,12 @@ resource "selectel_iam_serviceuser_v1" "deploy" {
   enabled  = true
 
   role {
+    role_name  = "member"
+    scope      = "project"
+    project_id = selectel_vpc_project_v2.main.id
+  }
+
+  role {
     role_name  = "s3.admin"
     scope      = "project"
     project_id = selectel_vpc_project_v2.main.id
